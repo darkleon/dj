66adf070a7d6dfbf8f7dff616760ba9752d860dc
root <root@ip-172-31-43-244.us-west-2.compute.internal>
2015-12-31 07:41:57 +0000

    fix 9

---
+++ 111.py
@@ -0,0 +1,15 @@
+import unittest
+import xmlrunner
+
+class TheTest(unittest.TestCase):
+
+    def testOne(self):
+        self.assertEquals(1, 1)
+    def testTwo(self):
+        self.assertEquals(2, 2)
+    def testThree(self):
+        self.assertEquals(3, 4)
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TheTest)
+    xmlrunner.XMLTestRunner().run(suite)
